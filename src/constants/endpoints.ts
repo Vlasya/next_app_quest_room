080d045a07bbd91d8cab0a04831b04a572d45cb2
Альбина <albina@My-Mac.local>
2024-05-09 15:46:56 +0300

    chanded base url

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,4 @@
-
-export const BASE_ENDPOINT='http://localhost:3001'
-export const GUESTS='/quests/'
-export const ORDERS='/orders/'
+// export const BASE_ENDPOINT='http://localhost:3001'
+export const BASE_ENDPOINT = 'next-app-quest-room.vercel.app';
+export const GUESTS = '/quests/';
+export const ORDERS = '/orders/';
